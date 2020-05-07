3104ba29e9704a2627533452037db431e24ca7c2
Olivia Wilson <olivia@rtwilson.com>
2020-05-07 20:56:42 +0100

    Sorted errors

--- colours.py
+++ colours.py
@@ -1,11 +1,14 @@
 from numpy import random
-COLOUR_LIST = [(255,255,255),
-               (255,255,0),
-               (255,0,0),
-               (0,255,0),
-               (0,0,255),
-               (255,0,255),
-               (0,255,255)]
+
+COLOUR_DICT = {"white":  (255,255,255),
+               "yellow": (255,255,0),
+               "red": (255,0,0),
+               "green": (0,255,0),
+               "blue": (0,0,255),
+               "magenta": (255,0,255),
+               "cyan": (0,255,255)}
+
+COLOUR_LIST = list(COLOUR_DICT.keys)
 
 def choose_random_colour():
-    return random.choice(COLOUR_LIST)
+    return COLOUR_DICT[random.choice(COLOUR_LIST)]
